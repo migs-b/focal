412411aecf60978ee3f78164a93b07657b64561b
migs-b <m.bacho104@gmail.com>
2022-09-20 20:36:40 -0700

    update

---
+++ dice-roller.js
@@ -0,0 +1,18 @@
+var favouriteMeal = "BREAKFAST";
+
+console.log(favouriteMeal.toLowerCase());
+
+// const rolledDice = function (num) {
+//   results = "";
+//   for (let i = 0; i < num; i++) {
+//     if (i === num - 1) {
+//       results += Math.floor(Math.random() * 6) + 1;
+//     } else {
+//       results += `${Math.floor(Math.random() * 6) + 1}, `;
+//     }
+//   }
+//   return results;
+// };
+
+// const numDice = 3;
+// console.log(`Rolled ${numDice} dice: ${rolledDice(numDice)}`);
---
+++ reverse.js
@@ -0,0 +1,8 @@
+const input = process.argv[2];
+
+if (input)
+  console.log(reverse(input));
+
+const reverse = function(original) {
+  return original.split('').reverseList().join('');
+};
